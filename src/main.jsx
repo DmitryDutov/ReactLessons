1896b2020077fe777e48b96d5cfd7140def281e6
DmitryDutov <dmitry_dutov@inbox.ru>
2024-08-18 11:52:38 +0300

    Заработало!

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,62 @@
 import { createElement } from "react";
 import { createRoot } from "react-dom/client";
+import { restaurants } from "/materials/mock.js";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
- root.render(
-   createElement(
-     "ul",
-     { className: "someClass", style: { color: "red" } },
-     [1, 2, 3, 4, 5].map((id) => createElement("li", {}, id))
-   )
- );
+//const names = restaurants.map(rest => rest.name);
 
-//root.render(
-//  <ul className='someClass' style={{ color: "red" }}>
-//    {[1, 2, 3, 4, 5].map((id) => createElement("li", {}, id))}
-//  </ul>
-//);
+//console.log(names);
+
+
+const getRestInfo = (restaurant) => {
+
+    return (
+        <div>
+            <h1>{restaurant.name}</h1>
+            <h2>Menu:</h2>
+            <ul>
+                {
+                    restaurant.menu.map
+                    (
+                        restMenu =>
+                        (
+                        <li key={restMenu.id}>{restMenu.name} = {restMenu.price}</li>
+                        )
+                    )
+                }
+            </ul>
+            <h2>Reviews:</h2>
+            <ul>
+                {
+                    restaurant.reviews.map
+                    (
+                        review =>
+                        (
+                        <li key={review.id}> Name: {review.user}, Text: {review.text}, Rate: {review.rating}</li>
+                        )
+                    )
+                }
+            </ul>
+            <hr/>
+        </div>
+    );
+};
+
+
+ //root.render(
+ //  createElement(
+ //    "ul",
+ //    { className: "someClass", style: { color: "red" } },
+ //    [1, 2, 3, 4, 5].map((id) => createElement("li", {}, id))
+ //  )
+ //);
+
+
+root.render(
+    createElement(
+        "div", {}, restaurants.map((resto) => getRestInfo(resto))
+    )
+);
 
