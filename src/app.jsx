fc7cdc454cdd247b6fe7ae87aa72e8d00c604900
DmitryDutov <dmitry_dutov@inbox.ru>
2024-08-21 23:33:27 +0300

    Добавил недостающие объекты в компонент

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,47 @@
+import React from "react";
 import {restaurants} from "./constants/mock.js"
 
 export const App = ({title}) => {
     return (
-            <div>
+            <React.Fragment>
                 <h1>{title}</h1>
                 <div>{restaurants.length}</div>
+
                 <ul>
-
-                {restaurants.map(({ name }) => 
-                    {
-                        return ( 
-                            <li key={name}> {name} </li>
-                        );
-                    }
-                )
-            }
-
+                {restaurants.map(({ id, name, menu, reviews}) => 
+                        {
+                            return ( 
+                                <li key={id}> 
+                                    <h2>
+                                        {name}
+                                    </h2>
+                                    <h3>Menu:</h3>
+                                    <div>
+                                        {menu.map(
+                                                restMenu =>
+                                                (
+                                                    <li key={restMenu.id}>{restMenu.name} = {restMenu.price}</li>
+                                                )
+                                            )
+                                        }
+                                    </div>
+                                    <h3>Review:</h3>
+                                    <div>
+                                        {reviews.map(
+                                                restReviw =>
+                                                (
+                                                    <li key={restReviw.user}>{restReviw.text} = {restReviw.rating}</li>
+                                                )
+                                            )
+                                        }
+                                    </div>
+                                </li>
+                            );
+                        }
+                    )
+                }
                 </ul>
-            </div>
+            </React.Fragment>
     );
 
 };
