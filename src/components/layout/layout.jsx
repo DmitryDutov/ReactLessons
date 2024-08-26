d0d777437ce696d5b9305262922d008df83207cb
DmitryDutov <dmitry_dutov@inbox.ru>
2024-08-26 19:56:08 +0300

    Заработал clear + progressBar

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,9 +1,11 @@
 import { Footer } from "../footer/footer"
 import { Header } from "../header/header";
+import { ProgressBar } from "../progreessbar/progressbar";
 
 export const Layout = ({ children }) => {
     return (
         <div>
+            <ProgressBar />
             <Header />
             {children}
             <Footer />
