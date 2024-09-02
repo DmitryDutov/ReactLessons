b9e40fbd5f27911ff99164abc92ec67075b08cc4
DmitryDutov <dmitry_dutov@inbox.ru>
2024-09-02 15:07:22 +0300

    Добавил рамку в doc

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,13 @@
 import { RestaurantPage } from "./components/restaurant-page/restaurant-page";
 import { Layout } from "./components/layout/layout";
+import "./App.css"
 
 export const App = () => {
     return (
-        <Layout>
-            <RestaurantPage title='restaurants app' />
-        </Layout>
+        <div id="doc">
+            <Layout>
+                <RestaurantPage title='restaurants app' />
+            </Layout>
+        </div>
     );
 };
